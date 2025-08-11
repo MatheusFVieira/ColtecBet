cff020ad46efca9e42ac80293993727ec4b33ce0
MatheusFVieira <160733404+MatheusFVieira@users.noreply.github.com>
2025-08-11 16:32:08 -0300

    Fix layout

--- coltec-bet-react/src/components/Layout.jsx
+++ coltec-bet-react/src/components/Layout.jsx
@@ -16,7 +16,7 @@ function Layout() {
         <>
             <header>
                 <div className="logo">
-                    <Link to="/"><img src="/logo1.jpg" id="logo1" alt="Logo Coltec.BET"/></Link>
+                    <Link to="/"><img src="../assets/logo1.jpg" id="logo1" alt="Logo Coltec.BET"/></Link>
                 </div>
 
                 <button className="menu-toggle" onClick={toggleMenu}>
@@ -45,20 +45,20 @@ function Layout() {
             </main>
             
             <footer>
-                <p>&copy; 2024 Coltec.BET. Todos os direitos reservados.</p>
                 <div className="footer-spacer"></div>
-                <div className="footer-center">
+
+                <div className="footer-main-content">
+                    <p>&copy; 2025 Coltec.BET. Todos os direitos reservados.</p>
                     <div className="footer-links">
-                        <Link to="/suporte">Suporte</Link>
-                        <span>|</span>
-                        <Link to="/aviso-legal">Aviso Legal</Link>
+                        <Link to="/aviso-legal">Aviso Legal ⚠️</Link>
                     </div>
                 </div>
+
                 <div className="footer-partner">
-                    <img src="/jumetos-logo.png" alt="Jumetos Oficial Partner" className="partner-logo" />
+                    <img src="../assets/jume.jpg" alt="Jumentus Official Partner" className="partner-logo" />
                     <div className="partner-text">
-                        <span>Jumetos</span>
-                        <span>Oficial</span>
+                        <span>Jumentus</span>
+                        <span>Official</span>
                         <span>Partner</span>
                     </div>
                 </div>
